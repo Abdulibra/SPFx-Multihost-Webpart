5e43024e08f73f96207f85565428dd29434734bc
Abdullahi Ibrahim Ahmed <abdullahi.ibrahim.ahmed@inmeta.no>
2023-06-16 23:23:26 +0200

    added Multihost webpart

---
+++ src/webparts/spFxBeyondSp/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISpFxBeyondSpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SpFxBeyondSpWebPartStrings' {
+  const strings: ISpFxBeyondSpWebPartStrings;
+  export = strings;
+}
